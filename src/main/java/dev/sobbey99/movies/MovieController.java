d782444ad77b7f9712cabaf60d3dcfb56da3771f
sobbey99 <sobbey99@gmail.com>
2023-07-10 17:47:38 +0300

    controler,servis,repository,findAll of collection

---
+++ src/main/java/dev/sobbey99/movies/MovieController.java
@@ -0,0 +1,24 @@
+package dev.sobbey99.movies;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RestController
+@RequestMapping("/api/v1/movies")
+public class MovieController {
+
+	@Autowired
+	private MovieService movieService;
+
+	@GetMapping
+	public ResponseEntity<List<Movie>> getAllMovies() {
+		return new ResponseEntity<List<Movie>>(movieService.allMovies(), HttpStatus.OK);
+	}
+}
---
+++ src/main/java/dev/sobbey99/movies/MovieRepository.java
@@ -0,0 +1,10 @@
+package dev.sobbey99.movies;
+
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MovieRepository extends MongoRepository<Movie, ObjectId> {
+
+}
